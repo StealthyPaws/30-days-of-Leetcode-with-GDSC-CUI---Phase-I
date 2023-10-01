566604e344811fbd5acf376f68184bf8841e2b99
StealthyPaws <134377530+StealthyPaws@users.noreply.github.com>
2023-10-01 23:50:54 +0500

    Update 1470-shuffle-the-array.cpp

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    int finalValueAfterOperations(vector<string>& operations) {
-        int X = 0; 
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int s = nums.size();
+        int k = 0;
         
-        for (const string& op : operations) {
-            if (op == "++X" || op == "X++") {
-                X++;  
-            }
-            else {
-                X--;  
+        // Create two separate arrays for even and odd elements
+        vector<int> odd(s/2);
+        vector<int> even(s/2);
+        
+        for (int i = 0; i < s/2; i++) {
+            odd[i] = nums[i];
+            k++;
+        }
+        for (int i = 0; i < s/2; i++) {
+            even[i] = nums[k];
+            k++;
+        }
+        
+        k = 0;
+        int l = 0;
+        
+        // Interleave elements from even and odd arrays into nums
+        for (int i = 0; i < s; i++) {
+            if (i % 2 == 0) {
+                nums[i] = odd[k];
+                k++;
+            } else {
+                nums[i] = even[l];
+                l++;
             }
         }
         
-        return X;  
+        return nums;
     }
 };
